7947bbcbe2039ab0ddb132f3155ace867eec52b9
LiHan <encorex32268@gmail.com>
2025-01-10 22:16:46 +0900

    setup

---
+++ app/src/main/java/com/lihan/drinkorderapp/feature/core/domain/Drink.kt
@@ -0,0 +1,28 @@
+package com.lihan.drinkorderapp.feature.core.domain
+
+import androidx.annotation.DrawableRes
+import com.lihan.drinkorderapp.R
+
+data class Drink(
+    val name: String,
+    val description: String?=null,
+    val price: Long,
+    val type: DrinkType,
+    @DrawableRes val image: Int
+)
+
+sealed interface DrinkType {
+    data object Coffee: DrinkType
+    data object Tea: DrinkType
+}
+
+val drinks = listOf<Drink>(
+    Drink(name = "Mocha", description = "Espresso with chocolate and steamed milk, often topped with whipped cream.", price = 160, type = DrinkType.Coffee , image = R.drawable.img_mocha),
+    Drink(name = "Macchiato", description = "Espresso with a small amount of milk or foam.", price = 125, type = DrinkType.Coffee, image = R.drawable.img_macchiato),
+    Drink(name = "Latte", description = "Espresso with a generous amount of steamed milk and a small layer of foam.", price = 150, type = DrinkType.Coffee, image = R.drawable.img_latte),
+    Drink(name = "Ristretto", description = "A short and intense shot of espresso made with less water.", price = 110, type = DrinkType.Coffee, image = R.drawable.img_ristretto),
+    Drink(name = "Café con Leche", description = "Similar to a latte, made with strong coffee and scalded milk.", price = 150, type = DrinkType.Coffee, image = R.drawable.img_cafe_leche),
+    Drink(name = "Café Cubano", description = "Espresso with sugar, creating a sweet and strong coffee.", price = 145, type = DrinkType.Coffee, image = R.drawable.img_mocha),
+    Drink(name = "Café Bombón", description = "Espresso with sweetened condensed milk.", price = 175, type = DrinkType.Coffee, image = R.drawable.img_cafe_bombon),
+    Drink(name = "Café Cortado", description = "Espresso cut with a small amount of warm milk to reduce the acidity.", price = 140, type = DrinkType.Coffee, image = R.drawable.img_cafe_cortado)
+)
---
+++ app/src/main/java/com/lihan/drinkorderapp/feature/core/presentation/Icons.kt
@@ -0,0 +1,98 @@
+package com.lihan.drinkorderapp.feature.core.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.lihan.drinkorderapp.R
+
+
+val AddIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.add)
+
+val AddCircleIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.add_circle)
+
+val AddSquareIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.add_square)
+
+val CloseCircleIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.close_circle)
+
+val CoffeeIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.coffee)
+
+val DislikeIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.dislike)
+
+
+val EmptyWalletTimeIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.empty_wallet_time)
+
+val FilterSearchIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.filter_search)
+
+val HeartIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.heart)
+
+val HomeIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.home)
+
+
+val LikeIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.like)
+
+val MessageNotifyIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.message_notif)
+
+val MinusIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.minus)
+
+val MinusCircleIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.minus_cirlce)
+
+val MinusSquareIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.minus_square)
+
+val SearchNormalIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.search_normal)
+
+val SearchZoomInIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.search_zoom_in)
+
+val SearchZoomOutIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.search_zoom_out)
+
+val SnowIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.snow)
+
+val StarIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.star)
+
+val UserIcon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.user)
+
+val Wind2Icon: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.wind_2)
+
